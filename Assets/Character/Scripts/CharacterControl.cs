0bf5340a0ed3e076d24d915c733de7622e3d846e
Egor Knyazev <egorknz@gmail.com>
2024-10-07 19:14:57 +0200

    Add character animations

--- Assets/Character/Scripts/CharacterControl.cs
+++ Assets/Character/Scripts/CharacterControl.cs
@@ -7,21 +7,26 @@ public class CharacterControl : MonoBehaviour
     private float _moveSpeed = 5;
     [SerializeField]
     private Rigidbody2D _character;
+    [SerializeField]
+    private Transform _characterSprite;
+    [SerializeField]
+    private Animator _animator;
+
     private CharacterInputAction _controls;
     private Vector2 _movementInput;
     private void Awake()
     {
         _controls = new CharacterInputAction();
 
-        _controls.Movement.Up.performed += _ => _movementInput.y = _moveSpeed;
-        _controls.Movement.Down.performed += _ => _movementInput.y = -_moveSpeed;
-        _controls.Movement.Right.performed += _ => _movementInput.x = _moveSpeed;
-        _controls.Movement.Left.performed += _ => _movementInput.x = -_moveSpeed;
+        _controls.Movement.Up.performed += _ => MoveUp();
+        _controls.Movement.Down.performed += _ => MoveDown();
+        _controls.Movement.Right.performed += _ => MoveRight();
+        _controls.Movement.Left.performed += _ => MoveLeft();
 
-        _controls.Movement.Up.canceled += _ => _movementInput.y = 0;
-        _controls.Movement.Down.canceled += _ => _movementInput.y = 0;
-        _controls.Movement.Right.canceled += _ => _movementInput.x = 0;
-        _controls.Movement.Left.canceled += _ => _movementInput.x = 0;
+        _controls.Movement.Up.canceled += _ => Stop(true);
+        _controls.Movement.Down.canceled += _ => Stop(true);
+        _controls.Movement.Right.canceled += _ => Stop(false);
+        _controls.Movement.Left.canceled += _ => Stop(false);
     }
 
     private void OnEnable()
@@ -30,6 +35,43 @@ public class CharacterControl : MonoBehaviour
     private void OnDisable()
         => _controls.Disable();
 
-    private void Update()
+    private void FixedUpdate()
         => _character.velocity = _movementInput;
+
+    private void MoveUp()
+    {
+        _movementInput.y = _moveSpeed;
+        _characterSprite.eulerAngles = Vector3.zero;
+        _animator.SetBool("IsMoving", true);
+    }
+
+    private void MoveDown() {
+        _movementInput.y = -_moveSpeed;
+        _characterSprite.eulerAngles = new Vector3(0, 0, 180);
+        _animator.SetBool("IsMoving", true);
+    }
+
+    private void MoveLeft()
+    { 
+        _movementInput.x = -_moveSpeed;
+        _characterSprite.eulerAngles = new Vector3(0, 0, 90);
+        _animator.SetBool("IsMoving", true);
+    }
+
+    private void MoveRight()
+    { 
+        _movementInput.x = _moveSpeed;
+        _characterSprite.eulerAngles = new Vector3(0, 0, -90);
+        _animator.SetBool("IsMoving", true);
+    }
+
+    private void Stop(bool IsVertical)
+    {
+        if (IsVertical)
+            _movementInput.y = 0;
+        else 
+            _movementInput.x = 0;
+
+        _animator.SetBool("IsMoving", false);
+    }
 }
